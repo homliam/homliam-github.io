c0ecc1d5f0fb0c04450eeb4279890a9a4269dc07
homliam <homliam@hotmail.com>
2019-04-03 18:46:21 +0800

    Site updated: 2019-04-03 18:46:20

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","9ae6208ca8a80b17d837f0e01c7deb08"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
