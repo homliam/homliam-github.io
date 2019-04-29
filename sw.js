7d4f94f793dfea1af5036f7a3ae96a466a660c68
id_rsa <homliam@hotmail.com>
2019-04-30 01:06:40 +0800

    Site updated: 2019-04-30 01:06:34

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","7714781db343e0bedbdba036473e3581"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
